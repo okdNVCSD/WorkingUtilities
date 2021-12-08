53c8293b5b7e0b8994b11bf64d7dfdbfdb284d65
okdNVCSD <73609811+okdNVCSD@users.noreply.github.com>
2021-12-08 08:47:34 -0800

    Update dplyr_filtering_Any_Column_Satisfies.R

--- dplyr_filtering_Any_Column_Satisfies.R
+++ dplyr_filtering_Any_Column_Satisfies.R
@@ -1,6 +1,10 @@
 # filtering rows for that any columns satisfies a given condition. 
 # 1. Example 1. A tidy sample that requires a helper function that takes the rowSum of boolean array returned from condition .fns applied to each column. 
 #      This is an example shown in tidyverse dplyr page. 
+#     **** One could infer that across applies a function to each of the applicable columns and returns a vector, logical, numeric, or character? So by applying logical function,
+#       the result of checking if any column satisfied the given condition could be checked, since if any of them did, the row-sum will return a sum bigger than zero. If none
+#       of the columns satisfied the condition the row-sum will equal to 0. 
+
 rowAny <- function(x) {
   rowSums(x) > 0      ## the dataframe df must be operating in rowwise() mode to make this work. x is n boolean vector when .fns is applied to each column of a df's row.
 }
