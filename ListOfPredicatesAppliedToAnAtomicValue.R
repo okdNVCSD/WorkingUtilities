91d8a84a7a6d5bd741e4d9d8a7ae7a111479d88c
okdNVCSD <73609811+okdNVCSD@users.noreply.github.com>
2021-12-14 11:58:01 -0800

    Update ListOfPredicatesAppliedToAnAtomicValue.R

--- ListOfPredicatesAppliedToAnAtomicValue.R
+++ ListOfPredicatesAppliedToAnAtomicValue.R
@@ -3,8 +3,35 @@
 # that could fulfill the same need. The return value could a vector or list of logicals, which could be reduced down to a single boolean with reduce(`&`) or reduce(`|`)
 
 # The idea is to first create a list of predicate functions ( ie is.na, isLessThanTwo, is.numeric, etc ) 
-isLessThanTwo <- function(x) {
-  
+isLessThan <- function(x, y) {
+  x < y
 }
+isLessThan2 <- function(x) {
+  isLessThan(x, 2)
+}
+isMultipleOf10 <- function(x) {
+  x %% 10 == 0
+}
+cmbPredicates <- list(is.na, isLessThan2, isMultipleOf10) 
 
-cmb <- list(is.na, 
+# The list of predicate functions could be applied to a value. 
+# The below two will return a vector ( because of sapply ) the same length of cmbPredicates...
+lg1 <- sapply(cmbPredicates, mapply, 5)
+lg2 <- sapply(cmbPredicates, function(x) sapply(5, x))
+
+>%lg1
+[1]  TRUE FALSE FALSE
+
+# Passing a list or vector of values will return a matrix. 
+aa2 <- list(10, NA, 3) 
+sapply(cmbPredicates, mapply, aa2)              
+[1]  [,1]  [,2]  [,3]
+[1,]  TRUE FALSE  TRUE
+[2,] FALSE    NA    NA
+[3,]  TRUE FALSE FALSE
+              
+# This could be passed to as.data.frame() and reduced to a vector of booleans, one for each input element. 
+sapply(cmb, mapply, aa2) %>% as.data.frame() %>% reduce(`|`)
+[1] TRUE   NA TRUE
+              
+              
