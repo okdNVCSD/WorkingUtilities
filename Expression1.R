54a6e43a9359d8ccfbeb5e2e4fb863e92681d7c5
okdNVCSD <73609811+okdNVCSD@users.noreply.github.com>
2022-01-26 08:35:29 -0800

    Create Expression1.R

---
+++ Expression1.R
@@ -0,0 +1,11 @@
+# From a column containing vector string, create series of dummy columns using expr evaluation. 
+
+# 
+tmp1s <- c("test1", "test2", "test3", "test4")
+
+#  dfc1$End_splits column contais a vector such as c("test3", "test1"), a subset of tmp1s...
+
+tmp1_vars <- tmp1s %>% purrr::map(~ expr(ifelse(like(End_splits, !!.x), 1, 0))) %>% 
+  purrr::set_names(paste0("tmp1_", gsub("\\s|/", "", tmp1s)))
+
+mutate(dfc1, !!!tmp1_vars) %>% View()
