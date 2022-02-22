6c6f2dbf461bb6e50d2817664ea6c88618cd296e
okdNVCSD <73609811+okdNVCSD@users.noreply.github.com>
2022-02-22 15:06:43 -0800

    Update vectorElementsRatio.R

--- vectorElementsRatio.R
+++ vectorElementsRatio.R
@@ -6,3 +6,16 @@ x <- c(
 )
 
 prop.table(table(x)) %>% round(2)
+
+
+
+x <- c(
+  c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1),
+  c(3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3),
+  c(2,2)
+)
+table(x) %>% class()
+table(x) %>% as_tibble() %>% ggplot(aes(x=x, y=n)) + geom_col()
+prop.table(table(x)) %>% round(2)
+
+
